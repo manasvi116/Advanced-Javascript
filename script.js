e4269e4a65d8d4d3aa3ce7115645fe6967203636
manasvi arora <manasvi925@gmail.com>
2025-04-04 10:28:27 +0530

    Functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// function sayHello() {
+//     let you = prompt("What's your name?");
+//     console.log("Hello " + you + "!");
+// }
+//sayHello();
+
+// let varContainingFunction=function(){
+//     let varInFunction="I'm in a function.";
+//     console.log("hi there!",varInFunction);
+// };
+// varContainingFunction();
+
+// let TextDecoderStream(para1,para2){
+//     return para1+" "+para2;
+// }
+// const arg1="argument1";
+// const arg2="argument2";
+// tester(arg1,arg2);
+
+function addNumbers(num1, num2) {
+    return parseInt(num1) + parseInt(num2);
+}
+const result = addNumbers("10", "20");
+console.log(result);
