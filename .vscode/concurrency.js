a6efc1adce497068adfd9f82c2da2154b68f46e7
manasvi arora <manasvi925@gmail.com>
2025-05-02 12:24:48 +0530

    file uploaded

--- .vscode/concurrency.js
+++ .vscode/concurrency.js
@@ -2,15 +2,17 @@
 // CONCURRENCY 
 
 // callbacks
-// a fucntion pass as an argument in another function 
+// a fucntion passed as an argument in another function and which is then called when the rest of the initial function is finished . 
 
 // function doSomething(callback){
 //     callback();
+//     console.log("hello manasvi");
 // }
 // function sayHi(){
 //     console.log("hi");
 // }
 // doSomething(sayHi);
+// 
 
 // function jugde(grade){
 //     switch (true){
@@ -67,8 +69,10 @@
 // with promises we can  organize the sequnece of ourcode in a slighlty easier way 
 
 // this function need two parameters and both of that are again a callback and we have called then resolve and reject 
-// when resolve is called the promise is presumed to be successful and whatever is bw the arros is returmed and used as input for the then method on the promise object . if reject is called the promise failed and then catch method on the promise object (if present) is executed with the argument of the reject funtion .
+// when resolve is called the promise is presumed to be successful and whatever is bw the arros is returned and used as input for the then() method on the promise object . if reject is called the promise failed and then catch() method on the promise object (if present) is executed with the argument of the reject funtion .
 
+// resolve - .then
+// reject - .catch 
 
 // let promise = new Promise(function (resolve, reject) {
 //     let x = 20;
@@ -87,33 +91,98 @@
 //         console.log("Error:", error);
 //     }
 // );
-const promise = new Promise((resolve, reject) => {
-    // Initial resolution of the promise
-    resolve("success!");
-})
+// const promise = new Promise((resolve, reject) => {
+//     // Initial resolution of the promise
+//     resolve("success!");
+// })
 
-.then(value => {
-    console.log(value); // Logs: success!
-    return "we";
-})
-.then(value => {
-    console.log(value); // Logs: we
-    return "can";
-})
-.then(value => {
-    console.log(value); // Logs: can
-    return "chain";
-})
-.then(value => {
-    console.log(value); // Logs: chain
-})
+// .then(value => {
+//     console.log(value); // Logs: success!
+//     return "we";
+// })
+// .then(value => {
+//     console.log(value); // Logs: we
+//     return "can";
+// })
+// .then(value => {
+//     //   console.log(value); // Logs: we
+//     //   return "can";
+// })
+// .then(value => {
+//     console.log(value); // Logs: can
+//     return "chain";
+// })
+// .then(value => {
+//     console.log(value); // Logs: chain
+// })
 
-// Error handling block (in case any .then throws an error or reject is called)
-.catch(error => {
-    console.log("Error:", error);
-});
+// // Error handling block (in case any .then throws an error or reject is called)
+// .catch(error => {
+//     console.log("Error:", error);
+// });
 
 
 
 
 // async and await 
+
+// with the async keyword we can make function retrun a promise (non concurrent code). this make the promise nicer to read and look a lot like sybchronius code . 
+// more powerful await keyword to wait until the promise is done awiat only works in an asynchronous function
+
+// function saySomething(x){
+//     return new Promise(resolve=> {
+//         setTimeout(() =>{
+//             resolve("something " + x);
+//         }, 2000);
+//     });
+// }
+// async function talk(x) {
+//     const words = await saySomething(x);
+//     // without await - pending 
+//     console.log(words);
+    
+// }
+// talk(2);
+// talk(4);
+// talk(8);
+
+// after 2 seconds the theree functions are done at the same Time.
+// executed in 2 seconds
+// without await each function will take 2 seconds and total time will be 6 seconds
+
+// EVENT LOOP 
+// js is a single threaded language. A thread in this context means a path of executon .I f there is only a single path this means the task has to wait for one another and only the one thing can happen at at time .
+// This single executor is the event loop . Its a process thet executes the actual TextTrackList. well even though js is single threaded it does not mean that it cannot outsource some task and wait for them to come back . this is exactly how the js manages to do things in  a multithreaded manner.
+
+// CALL STACK AND CALLBACK QUEUE 
+// THE EVENT LOOP IS A PROCESS THAT IS CONSTANTLY MONITORING THIS CALL STACK , AND WHENEVER THERE ARE TASK TO DO THE EVENT LOOOP DOES THEM ONE BY ONE AND TASK ON TOP GETS EXECTUED AT FIRST 
+
+
+console.log();
+add(4,5); // <- This runs but doesn't show the result
+
+function add(x,y){
+    return x + y;
+}
+
+console.log("hi there");
+setTimeout(() => console.log("sorry i am late"), 1000);
+console.log(add(4,5));
+
+function add(x,y){
+    return x + y;
+}
+ 
+// until and unless callstack will become empty , the callback queue will not be called
+// the setTimeout() task gets outsourced to the browser's web api .
+// when the ccallstack is empty the event loop will check the callback queue for work to do . if there are any callabcks waiting they will be executed one by one after every action the veent loop will check the call stack for wrok first 
+
+console.log("hi there");
+setTimeout(() => console.log("soory i am late"), 0);
+console.log(add(4,5));
+
+function add(x,y){
+    return x + y;
+}
+
+// this will output the exact same thing setTimeout will also be outsourced when the timer is at 0 , the callback is placed in the callback queue right away but the event loop wont even check the callback queue until the callstack is empty  
--- .vscode/tempCodeRunnerFile.js
+++ .vscode/tempCodeRunnerFile.js
@@ -1,4 +1,7 @@
-setinterval (encourage , 500);
-//     function encourage() {
-//         console.log("you are doing graet , keep going ");
-//     }
+
+console.log("hi there");
+add(4,5); // <- This runs but doesn't show the result
+
+function add(x,y){
+    return x + y;
+}
